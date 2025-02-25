045b76fc0ea1306fa08d9fed9989af4da2264874
rece212 <rwanvig@varsitycollege.co.za>
2025-02-25 16:02:20 +0200

    Add project files.

---
+++ MVCStudents/Models/StaffContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCStudents.Models
+{
+    public class StaffContext
+    {
+        public static List<Staff> staffObject = new List<Staff>();
+        public StaffContext() 
+        {
+            if (staffObject.Count == 0)
+            {
+                staffObject.Add(new Staff(1,"Max","Baby staff member","1234","2004"));
+                staffObject.Add(new Staff(2, "Chop", "Is a baby", "Can't remember", "2014"));
+                staffObject.Add(new Staff(3, "Kyle", "Mini staff member", "Kyle", "1997"));
+            }
+
+        }
+    }
+}
